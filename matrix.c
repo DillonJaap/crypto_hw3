bf8f4fafe61656e9c395b8d830a417721c55e474
DillonJaap <47013506+DillonJaap@users.noreply.github.com>
2020-03-31 11:11:06 -0600

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -33,6 +33,14 @@ matrix* init_matrix(uint8_t rows, uint8_t cols, uint8_t* data)
 	return m;
 }
 
+void free_matrix(matrix* m)
+{
+	for (uint8_t i = 0; i < m->rows; i++)
+		free(m->t[i]);
+	free(m->t);
+	free(m);
+}
+
 void set_matrix(matrix* m, uint8_t* data)
 {
 	for (uint8_t i = 0; i < m->rows; i++)
@@ -93,9 +101,7 @@ void mult_matrix(matrix* dest, matrix* L, matrix* R)
 	for (uint8_t i = 0; i < R->cols; i++)
 	{
 		for (uint8_t j = 0; j < L->rows; j++)
-		{
 			dest->t[i][j] = sum_Lrow_Rcol(L, R, i, j);
-		}
 	}
 }	
 
